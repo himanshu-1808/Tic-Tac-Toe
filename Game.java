3028562f684c2a3fbe16b29e220c202411e10c6d
Himanshu <75906597+himanshu-1808@users.noreply.github.com>
2021-05-14 16:20:41 +0530

    Game base done

--- Game.java
+++ Game.java
@@ -1,5 +1,22 @@
+//import java.util.*;
 class Game{
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        char arr[][] ={{' ','|',' ','|',' '},
+                        {'-',' ','-',' ','-'},
+                        {' ','|',' ','|',' '},
+                        {'-',' ','-',' ','-'},
+                        {' ','|',' ','|',' '}
+                        
+        };
+        
+        show(arr);
+    }
+    public static void show(char arr[][]){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
